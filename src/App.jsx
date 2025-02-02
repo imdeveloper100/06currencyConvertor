47e47d81991b092392a38cdeeb6499dde2a9f300
awais-rehman-octate <awais.rehman@octate.ooo>
2025-02-02 13:28:11 +0500

    enhancement

--- src/App.jsx
+++ src/App.jsx
@@ -48,10 +48,21 @@ function App() {
             <div className="relative w-full h-0.5">
               <button
                 type="button"
-                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5 hover:bg-blue-700 active:bg-blue-800 active:scale-90 transition-all duration-300"
                 onClick={swap}
               >
-                swap
+                <svg
+                  viewBox="64 64 896 896"
+                  focusable="false"
+                  data-icon="swap"
+                  width="1.5em"
+                  height="1.5em"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="hover:cursor-pointer transform hover:scale-110 transition-transform duration-200"
+                >
+                  <path d="M847.9 592H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h605.2L612.9 851c-4.1 5.2-.4 13 6.3 13h72.5c4.9 0 9.5-2.2 12.6-6.1l168.8-214.1c16.5-21 1.6-51.8-25.2-51.8zM872 356H266.8l144.3-183c4.1-5.2.4-13-6.3-13h-72.5c-4.9 0-9.5 2.2-12.6 6.1L150.9 380.2c-16.5 21-1.6 51.8 25.1 51.8h696c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8z"></path>
+                </svg>
               </button>
             </div>
             <div className="w-full mt-1 mb-4">
